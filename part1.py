fc69a5fe36ada1dff979aea3e4223a3a7ff51b2a
myname21-meet <myname21@meet.mit.edu>
2019-07-28 11:12:14 -0400

    a

--- part1.py
+++ part1.py
@@ -1,13 +1,14 @@
 # Part 1 of the Python Review lab.
 
 def hello_world():
-	return(hello world)
+	return("hello world")
 
 def greet_by_name(name):
-	return("hello" name)
+	return("hello "+ name)
 
 def encode(x):
-	return()
+	return(x*3953531)
 
 def decode(y):
-	pass
+	return(y/3953531)
+	
